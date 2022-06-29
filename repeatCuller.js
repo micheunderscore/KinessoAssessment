1f33a0ecd83605128561a06027d4c2f09b6a989f
Miche_ <51939599+micheunderscore@users.noreply.github.com>
2022-06-29 14:46:59 +0800

    update(repeat-culler): updated ans

--- repeatCuller.js
+++ repeatCuller.js
@@ -2,17 +2,19 @@
 var testString = "Lorem Ipsum is simply dummy text";
 // Feel free to change it ^^^
 
+// For the question, I'm assuming that the method needs to
+// show only the first appearance of characters and remove
+// any further duplicates. Spaces are also included since
+// the question did not specify what qualified as a "character".
+
 // I'm just using an arrow function for defining
 // repeatCuller() because I'm used to it and it's
 // faster to type.
 repeatCuller = (inString) => {
   // Character bank object to map all of the characters to
   let charBank = {};
-
   // for loop going through all of the characters in the string
   for (const char of inString) {
-    // skip the iteration if it's a space
-    if (char == " ") continue;
     // Increment the current value or add to 0 if there is no record
     charBank[char] = (charBank[char] || 0) + 1;
   }
